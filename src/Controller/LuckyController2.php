b8c59840e427079d22f70c11555d50b52d82a83c
Gustavo Julián <gustavo.julian@clabsa.com.mx>
2021-05-11 12:27:59 -0500

    Annotations

---
+++ src/Controller/LuckyController2.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController2
+{
+
+    /**
+     * @Route("lucky/number2")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+        return new Response(
+            '<html><body>Lucky number2: '.$number.'</body></html>'
+        );
+    }
+}
